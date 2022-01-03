aa4ca2ba877a6159cd3871a1408a2af70a69c2a5
lucas <lucasmaciel6690@gmail.com>
2022-01-02 23:14:31 -0300

    modificações complementares

--- src/templates/chat/index.jsx
+++ src/templates/chat/index.jsx
@@ -10,6 +10,7 @@ import closeChat from '../../assets/svg/CloseChat.svg'
 export const Chat = () => {
     const [texto, setTexto] = React.useState([]);
     const [tex, setTex] = React.useState([]);
+    const [fecharChart, setFecharChart] = React.useState(false);
     const divRef = React.useRef();
     React.useLayoutEffect(() => {
         divRef.current.scrollTop = divRef.current.scrollHeight;
@@ -31,57 +32,66 @@ export const Chat = () => {
     }
     return (
         <>
-            <div id="chat" style={styles}>
-                <div id="chat-header">
-                    <div id="chat-header-Itens">
-                        <div id="chat-header-itens-textos">
-                            <p id="nomeChat">BALINU</p>
-                            <p id="descricaoChat">Assistente Virtual da PROPPG</p>
-                        </div>
-                        <div id="chat-header-itens-img">
-                            <img src={closeChat} alt="Fechar" id="chat-header-itens-img-item"/>
+            <div id="all-chat">
+                {!fecharChart && 
+                <div id="chat" style={styles}>
+                    <div id="chat-header">
+                        <div id="chat-header-Itens">
+                            <div id="chat-header-itens-textos">
+                                <p id="nomeChat">BALINU</p>
+                                <p id="descricaoChat">Assistente Virtual da PROPPG</p>
+                            </div>
+                            <div id="chat-header-itens-img">
+                                <img src={closeChat} alt="Fechar" id="chat-header-itens-img-item"/>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div id="chat-body" ref={divRef}>
-                    <div id="chat-body-itens">
-                            {texto.map((c) => {
-                                if(c.user){
-                                    return (
-                                        <>
-                                        <div id="chat-body-item-msg-user" key={c.user}>
-                                            <div id="chat-body-item-msg-user-texto">{c.user}</div>
-                                        </div>
-                                        </>
-                                    )
-                                }
-                                if(c.bot){
-                                    return (
-                                        <>
-                                            <div id="chat-body-item-msg-bot" key={c.bot}>
-                                                <div id="chat-body-item-msg-bot-foto">
-                                                    <img src={Avatar} id="chat-body-item-msg-bot-foto-img"/>
-                                                </div>
-                                                <div id="chat-body-item-msg-bot-texto">
-                                                    <p id="chat-body-item-msg-bot-texto-p">{c.bot}</p>
-                                                </div>
+                    <div id="chat-body" ref={divRef}>
+                        <div id="chat-body-itens">
+                                {texto.map((c) => {
+                                    if(c.user){
+                                        return (
+                                            <>
+                                            <div id="chat-body-item-msg-user" key={c.user}>
+                                                <div id="chat-body-item-msg-user-texto">{c.user}</div>
                                             </div>
-                                        </>
-                                    )
-                                }
-                            })}
-                    </div>
-                </div>
-                <div id="chat-footer">
-                    <div id="chat-footer-itens">
-                        <div id="chat-footer-itens-input">
-                            <input type="text" onKeyUp={(e)=> {if(e.keyCode === 13){handleClick()}}} id="chat-footer-itens-input-input" onChange={(e) => setTex(e.target.value)}></input>
+                                            </>
+                                        )
+                                    }
+                                    if(c.bot){
+                                        return (
+                                            <>
+                                                <div id="chat-body-item-msg-bot" key={c.bot}>
+                                                    <div id="chat-body-item-msg-bot-foto">
+                                                        <img src={Avatar} id="chat-body-item-msg-bot-foto-img"/>
+                                                    </div>
+                                                    <div id="chat-body-item-msg-bot-texto">
+                                                        <p id="chat-body-item-msg-bot-texto-p">{c.bot}</p>
+                                                    </div>
+                                                </div>
+                                            </>
+                                        )
+                                    }
+                                })}
                         </div>
-                        <div id="chat-footer-itens-send">
-                            <img src={Send} alt="send" id="chat-header-itens-img-item" onClick={() => handleClick()}/>
+                    </div>
+                    <div id="chat-footer">
+                        <div id="chat-footer-itens">
+                            <div id="chat-footer-itens-input">
+                                <input type="text" onKeyUp={(e)=> {if(e.keyCode === 13){handleClick()}}} id="chat-footer-itens-input-input" onChange={(e) => setTex(e.target.value)}></input>
+                            </div>
+                            <div id="chat-footer-itens-send">
+                                <img src={Send} alt="send" id="chat-header-itens-img-item" onClick={() => handleClick()}/>
+                            </div>
                         </div>
                     </div>
+                </div>}
+                {!fecharChart &&
+                <div id="chat-popup">
+                    <img src={Avatar} id="chat-popup-foto-img"/>
                 </div>
+                }
+                
             </div>
         </>
     );
